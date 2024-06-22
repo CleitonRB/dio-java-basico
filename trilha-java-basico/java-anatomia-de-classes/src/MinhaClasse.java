b71b0c1ea29b17407aba64135234c6a0a1392ad0
CleitonRB <cleiton619@outlook.com>
2024-06-22 20:46:33 -0300

    Imprimindo Nome

--- trilha-java-basico/java-anatomia-de-classes/src/MinhaClasse.java
+++ trilha-java-basico/java-anatomia-de-classes/src/MinhaClasse.java
@@ -2,8 +2,16 @@ public class MinhaClasse {
 
     public static void main (String [] args) {
 
-        System.out.print ("Olá Professor, adoro essa aula!");
+        String primeiroNome = "Cleiton";
+        String segundoNome = "Bandeira";
 
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
     
